be7d49ec34a9d5aefc37a0e3fe33ff77f6613b6b
Yulia Skorobogatova <sjs@smart.inreco.ru>
2022-02-21 17:47:22 +0300

    own react, nmp modules

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 // file: src/index.js
 // QndReact needs to be in scope for JSX to work
-console.log("????");
 
-// import QndReact from "./qnd-react";
-// import QndReactDom from "./qnd-react-dom";
+import QndReact from "./qnd-react";
+import QndReactDom from "./qnd-react-dom";
 
-// const App = (
-//     <div>
-//       <h1 className="primary">
-//         QndReact is Quick and dirty react
-//       </h1>
-//       <p>It is about building your own React in 90 lines of JavsScript</p>
-//     </div>
-// );
+const App = (
+    <div>
+      <h1 className="primary">
+        QndReact is Quick and dirty react
+      </h1>
+      <p>It is about building your own React in 90 lines of JavsScript</p>
+    </div>
+);
 
-// console.log(App);
+console.log(App);
 
-// QndReactDom.render(App, document.getElementById('root'));
+QndReactDom.render(App, document.getElementById('root'));
